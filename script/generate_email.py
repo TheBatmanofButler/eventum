0cd43d92b8acdd81d0dfcc4b06e42687f2e3316b
Ganesh Ravichandran <gr2483@columbia.edu>
2014-12-05 13:40:38 -0500

    python script that filters for this week's events

--- script/generate_email.py
+++ script/generate_email.py
@@ -7,10 +7,17 @@ def generate_events_email(emails_path):
 
 	connect('eventum')
 
-	events = Event.objects()
-	# .filter(start_date=[datetime.date.today(), datetime.date.today() + datetime.timedelta(days=7)])
+	today_weekday = datetime.date.today().weekday()
+
+	date_start = datetime.date.today()-datetime.timedelta(today_weekday+1)
+	print date_start, date_start+datetime.timedelta(7)
+
+	events = []
+	for each_event in Event.objects():
+		if date_start <= each_event.start_date <= date_start+datetime.timedelta(7):
+			events.append(each_event)
 
 	env = Environment(loader=PackageLoader('app', 'templates'))
 	template = env.get_template('email/weekly.html')
 
-	print template.render(events=events)
+	print template.render(events=events) 
